23ff8b0a1a933439d246e43baef3c24c6925f771
Ishika Gupta <gishika2909@gmail.com>
2026-08-13 15:08:57 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (83.31%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int c=0;
+        for(int i =0;i<nums.size();i++){
+            if(nums[i]!=0){
+                nums[c]=nums[i];
+                c++;
+            }
+            
+        }
+        while(c<nums.size()){
+            nums[c]=0;
+            c++;
+        }
+        
+        
+    }
+};
